5d2a9a22fcb48164fcf6f423eb420d17c1bab7cc
verazhen <vera.zhen63@gmail.com>
2022-04-18 21:58:11 +0800

    <feat> remove files

--- server/routes/report_route.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { wrapAsync, authentication } = require('../../util/util');
-
-const { getReport } = require('../controllers/report_controller');
-
-// For load testing (Not in API Docs)
-router.route('/report/payments').get(wrapAsync(getReport));
-
-module.exports = router;
