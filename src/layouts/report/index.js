6255712f4becdc208bd8ff16f29d4099f032b7a7
verazhen <vera.zhen63@gmail.com>
2022-04-22 15:34:32 +0800

    <feat> move task to different list

--- src/layouts/report/index.js
+++ src/layouts/report/index.js
@@ -63,22 +63,23 @@ function Tables() {
       //if not the same list
       const sourceColumn = columns[source.droppableId];
       const destColumn = columns[destination.droppableId];
-      const sourceItems = [...sourceColumn.items];
-      const destItems = [...destColumn.items];
+      const sourceItems = [...sourceColumn.tasks];
+      const destItems = [...destColumn.tasks];
       const [removed] = sourceItems.splice(source.index, 1);
 
       destItems.splice(destination.index, 0, removed);
-      setColumns({
-        ...columns,
-        [source.droppableId]: {
-          ...sourceColumn,
-          items: sourceItems,
-        },
-        [destination.droppableId]: {
-          ...destColumn,
-          items: destItems,
-        },
-      });
+      if (destination.index != 0) {
+        destItems[destination.index].orders =
+          destItems[destination.index - 1].orders + 1;
+      } else {
+        destItems[destination.index].orders =
+          destItems[destination.index + 1].orders - 1;
+      }
+
+      const newList = JSON.parse(JSON.stringify(lists));
+      newList[source.droppableId].tasks = sourceItems;
+      newList[destination.droppableId].tasks = destItems;
+      setLists(newList);
     } else {
       const list = lists[source.droppableId];
       const copiedItems = [...list.tasks];
