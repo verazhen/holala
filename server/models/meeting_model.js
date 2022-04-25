7e17df10894cebb3167286fccac59c19888a7edc
verazhen <vera.zhen63@gmail.com>
2022-04-25 11:21:57 +0800

    <api> close the room after owner authenticated

--- server/models/meeting_model.js
+++ server/models/meeting_model.js
@@ -1,22 +1,5 @@
 const { pool } = require("./mysqlcon");
 
-//get Meeting
-const getTasks = async (id) => {
-  const [lists] = await pool.query("SELECT * FROM lists WHERE kanban_id = ?", [
-    id,
-  ]);
-  let data = [];
-  for (let i = 0; i < lists.length; i++) {
-    const { id, title, orders } = lists[i];
-    const [tasks] = await pool.query(
-      `SELECT * FROM tasks WHERE list_id = ${id}`
-    );
-    data.push({ id, title, orders, tasks });
-  }
-
-  return data;
-};
-
 const getRoom = async ({ uid, kanbanId }) => {
   let roomId;
   const conn = await pool.getConnection();
@@ -52,6 +35,39 @@ const getRoom = async ({ uid, kanbanId }) => {
   }
 };
 
+const leaveRoom = async ({ uid, kanbanId }) => {
+  const conn = await pool.getConnection();
+  try {
+    await conn.query("START TRANSACTION");
+    const [[res]] = await pool.query(
+      `SELECT user_id,id FROM meetings WHERE kanban_id = ? AND end_dt IS NULL`,
+      [kanbanId]
+    );
+    let response;
+
+    if (res.user_id != uid) {
+      response = 1;
+    } else {
+      const [result] = await pool.query(
+        `UPDATE meetings SET end_dt=?, record=? WHERE id=? `,
+        [1, 1, res.id]
+      );
+      response = result;
+    }
+
+    await conn.query("COMMIT");
+
+    return response;
+  } catch (e) {
+    await conn.query("ROLLBACK");
+    console.log(e);
+    return false;
+  } finally {
+    await conn.release();
+  }
+};
+
 module.exports = {
   getRoom,
+  leaveRoom
 };
