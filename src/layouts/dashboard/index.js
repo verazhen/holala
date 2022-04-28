cd03e38226bd5e05bf2c983e921c29ef3bab4cab
verazhen <vera.zhen63@gmail.com>
2022-04-28 19:11:57 +0800

    <feat> report layout

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,23 +1,11 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
+import { useEffect, useState, useRef } from "react";
 
 // @mui material components
 import Grid from "@mui/material/Grid";
-
+import Form from "react-bootstrap/Form";
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
 
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
@@ -26,6 +14,7 @@ import Footer from "examples/Footer";
 import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
 import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
 import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
+import MixedChart from "examples/Charts/MixedChart";
 
 // Data
 import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
@@ -35,8 +24,23 @@ import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
 import Projects from "layouts/dashboard/components/Projects";
 import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
 
+const inputStyle = {
+  height: "50px",
+  width: "100%",
+  border: 0,
+  borderRadius: "5px",
+  boxShadow:
+    "-0.01px -0.01px 0.5px 0.01px lightgrey,0.5px 0.5px 3px 0.5px lightgrey",
+  padding: "2px 20px",
+  color: "grey",
+};
+
 function Dashboard() {
-  const { sales, tasks } = reportsLineChartData;
+  const [age, setAge] = useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
 
   return (
     <DashboardLayout>
@@ -47,8 +51,8 @@ function Dashboard() {
             <MDBox mb={1.5}>
               <ComplexStatisticsCard
                 color="dark"
-                icon="weekend"
-                title="Bookings"
+                icon="assignment"
+                title="Total Tasks"
                 count={281}
                 percentage={{
                   color: "success",
@@ -61,8 +65,8 @@ function Dashboard() {
           <Grid item xs={12} md={6} lg={3}>
             <MDBox mb={1.5}>
               <ComplexStatisticsCard
-                icon="leaderboard"
-                title="Today's Users"
+                icon="check_circle"
+                title="Finished Tasks"
                 count="2,300"
                 percentage={{
                   color: "success",
@@ -76,8 +80,8 @@ function Dashboard() {
             <MDBox mb={1.5}>
               <ComplexStatisticsCard
                 color="success"
-                icon="store"
-                title="Revenue"
+                icon="check_circle_outline"
+                title="Unfinished Tasks"
                 count="34k"
                 percentage={{
                   color: "success",
@@ -91,8 +95,8 @@ function Dashboard() {
             <MDBox mb={1.5}>
               <ComplexStatisticsCard
                 color="primary"
-                icon="person_add"
-                title="Followers"
+                icon="groups"
+                title="Meetings"
                 count="+91"
                 percentage={{
                   color: "success",
@@ -105,57 +109,124 @@ function Dashboard() {
         </Grid>
         <MDBox mt={4.5}>
           <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={4}>
+            <Grid item xs={12} md={6} lg={6}>
               <MDBox mb={3}>
-                <ReportsBarChart
-                  color="info"
-                  title="website views"
-                  description="Last Campaign Performance"
-                  date="campaign sent 2 days ago"
-                  chart={reportsBarChartData}
+                <Grid container spacing={3} wrap="nowrap">
+                  <Grid item alignSelf="center">
+                    <MDTypography
+                      variant="h6"
+                      style={{ verticalAlign: "middle" }}
+                    >
+                      Interval:{" "}
+                    </MDTypography>
+                  </Grid>
+                  <Grid item xs={11}>
+                    <Form.Select style={inputStyle} className="no-outline">
+                      <option>Daily</option>
+                      <option>Weekly</option>
+                      <option>Monthly</option>
+                    </Form.Select>
+                  </Grid>
+                </Grid>
+              </MDBox>
+
+              <MDBox mb={3}>
+                <MixedChart
+                  icon={{ color: "info", component: "leaderboard" }}
+                  title="Tasks Status"
+                  description="Tasks finished/unfinished by time"
+                  chart={{
+                    labels: [
+                      "Apr",
+                      "May",
+                      "Jun",
+                      "Jul",
+                      "Aug",
+                      "Sep",
+                      "Oct",
+                      "Nov",
+                      "Dec",
+                    ],
+                    datasets: [
+                      {
+                        chartType: "bar",
+                        label: "Finished Tasks",
+                        color: "dark",
+                        data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+                      },
+                      {
+                        chartType: "thin-bar",
+                        label: "unFinished Tasks",
+                        color: "primary",
+                        data: [60, 30, 300, 220, 500, 250, 400, 230, 500],
+                      },
+                      {
+                        chartType: "gradient-line",
+                        label: "Total Tasks",
+                        color: "secondary",
+                        data: [30, 90, 40, 140, 290, 290, 340, 230, 400],
+                      },
+                    ],
+                  }}
                 />
               </MDBox>
             </Grid>
-            <Grid item xs={12} md={6} lg={4}>
+            <Grid item xs={12} md={6} lg={6}>
               <MDBox mb={3}>
-                <ReportsLineChart
-                  color="success"
-                  title="daily sales"
-                  description={
-                    <>
-                      (<strong>+15%</strong>) increase in today sales.
-                    </>
-                  }
-                  date="updated 4 min ago"
-                  chart={sales}
+                <Grid container spacing={3} wrap="nowrap">
+                  <Grid item alignSelf="center">
+                    <MDTypography
+                      variant="h6"
+                      style={{ verticalAlign: "middle" }}
+                    >
+                      Range:{" "}
+                    </MDTypography>
+                  </Grid>
+                  <Grid item xs={11}>
+                    <Form.Select style={inputStyle} className="no-outline">
+                      <option>Today</option>
+                      <option>This Week</option>
+                      <option>This Month</option>
+                      <option>This Quarter</option>
+                    </Form.Select>
+                  </Grid>
+                </Grid>
+              </MDBox>
+
+              <MDBox mb={3}>
+                <MixedChart
+                  icon={{ color: "info", component: "leaderboard" }}
+                  title="Tasks Status"
+                  description="Tasks finished/unfinished by members"
+                  chart={{
+                    labels: [
+                      "Member1",
+                      "Member2",
+                      "Member3",
+                      "Member4",
+                      "Member5",
+                    ],
+                    datasets: [
+                      {
+                        chartType: "bar",
+                        label: "Finished Tasks",
+                        color: "primary",
+                        data: [60, 30, 300, 220, 500],
+                      },
+                      {
+                        chartType: "thin-bar",
+                        label: "unFinished Tasks",
+                        color: "primary",
+                        data: [60, 30, 300, 220, 500],
+                      },
+                    ],
+                  }}
                 />
               </MDBox>
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <MDBox mb={3}>
-                <ReportsLineChart
-                  color="dark"
-                  title="completed tasks"
-                  description="Last Campaign Performance"
-                  date="just updated"
-                  chart={tasks}
-                />
-              </MDBox>
-            </Grid>
-          </Grid>
-        </MDBox>
-        <MDBox>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={8}>
-              <Projects />
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <OrdersOverview />
             </Grid>
           </Grid>
         </MDBox>
       </MDBox>
-      <Footer />
     </DashboardLayout>
   );
 }
