93f32c6a9c18ae1bca1647e882bd9a8c0f3b13b6
verazhen <vera.zhen63@gmail.com>
2022-05-19 10:03:00 +0800

    <refactor> data validation of report_controller.js

--- server/controllers/report_controller.js
+++ server/controllers/report_controller.js
@@ -2,69 +2,78 @@ require("dotenv").config();
 const Report = require("../models/report_model");
 
 const getTasksAmount = async (req, res) => {
-  try {
-    const { kanbanId, status } = req.params;
-    let { range } = req.query;
-    if (!range) range = 7;
-    const response = await Report.getTasksAmount(kanbanId, status, range);
-    return res.json({
-      data: response,
+  const { kanbanId, status } = req.params;
+  let { range } = req.query;
+  if (!kanbanId || !status || !range) {
+    return res.status(400).send({
+      status_code: 400,
+      error: "Request Error: range and kanbanId and status are required.",
     });
-  } catch (error) {
-    console.log(error);
-    return { error };
   }
+
+  const response = await Report.getTasksAmount(kanbanId, status, range);
+  if (!response) {
+    return res.status(500).send({ status_code: 500, error: "Database Error" });
+  }
+
+  return res.json({
+    data: response,
+  });
 };
 
-
 const getMeetings = async (req, res) => {
-  try {
-    const { kanbanId } = req.params;
-    let { range } = req.query;
-    if (!range) range = 7;
-    const response = await Report.getMeetings(kanbanId, range);
-    return res.json({
-      data: response,
+  const { kanbanId } = req.params;
+  let { range } = req.query;
+
+  if (!kanbanId || !range) {
+    return res.status(400).send({
+      status_code: 400,
+      error: "Request Error: range and kanbanId are required.",
     });
-  } catch (error) {
-    console.log(error);
-    return { error };
   }
+
+  const response = await Report.getMeetings(kanbanId, range);
+  return res.json({
+    data: response,
+  });
 };
 
 const getTasksChart = async (req, res) => {
-  try {
-    const { kanbanId } = req.params;
-    let { range, interval } = req.query;
-    if (!range) range = 7;
-    if (!interval) interval = 1;
-    const response = await Report.getTasksChart(kanbanId, range, interval);
-    return res.json({
-      data: response,
+  const { kanbanId } = req.params;
+  let { range, interval } = req.query;
+
+  if (!kanbanId || !range || !interval) {
+    return res.status(400).send({
+      status_code: 400,
+      error:
+        "Request Error: range and kanbanId and status and interval are required.",
     });
-  } catch (error) {
-    console.log(error);
-    return { error };
   }
+
+  const response = await Report.getTasksChart(kanbanId, range, interval);
+  return res.json({
+    data: response,
+  });
 };
 
-
 const getLoading = async (req, res) => {
-  try {
-    const { kanbanId } = req.params;
-    let { range } = req.query;
-    if (!range) range = 7;
-    const response = await Report.getLoading(kanbanId, range);
-    return res.json({ data: response });
-  } catch (error) {
-    console.log(error);
-    return { error };
+  const { kanbanId } = req.params;
+  let { range } = req.query;
+
+  if (!kanbanId || !range) {
+    return res.status(400).send({
+      status_code: 400,
+      error: "Request Error: range and kanbanId and status are required.",
+    });
   }
+
+  const response = await Report.getLoading(kanbanId, range);
+  return res.json({ data: response });
 };
 
 module.exports = {
   getTasksAmount,
   getTasksChart,
   getMeetings,
-  getLoading
+  getLoading,
 };
